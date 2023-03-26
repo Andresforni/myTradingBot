f99566df8ee3288190f031c7d84ecb56fb10269e
Andresforni <123376922+Andresforni@users.noreply.github.com>
2023-03-26 08:22:23 -0300

    Update bot.ts

--- src/bot.ts
+++ src/bot.ts
@@ -9,7 +9,9 @@ import type { Variant as TextEffectVariant } from "./textEffects";
 const bot = new Bot(process.env.TELEGRAM_TOKEN || "");
 
 // Handle the /yo command to greet the user
-bot.command("yo", (ctx) => ctx.reply(`YASo ${ctx.from?.username}`));
+bot.command("yo", (ctx) => ctx.reply(`Yo ${ctx.from?.username}`));
+
+bot.command("we", (ctx) => ctx.reply(`we ${ctx.from?.username}`));
 
 // Handle the /effect command to apply text effects using an inline keyboard
 type Effect = { code: TextEffectVariant; label: string };
